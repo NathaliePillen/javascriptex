d1cb8fcd06f4048d35f7e07f7b39bf6db2c2a979
nathalie <pillennathalie@gmail.com>
2019-11-12 14:32:14 +0100

    updat ex2-1

--- 1.JavaScript-Basics/2.Challenge-Series/02-maths/01-calculator-one/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/02-maths/01-calculator-one/script.js
@@ -13,18 +13,64 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("addition").addEventListener("click", function() {
+        number1 = document.getElementById("op-one");
+        number2 = document.getElementById("op-two");
+
+        operator = parseInt(number1.value) + parseInt(number2.value);
+
+        console.log(operator);
+
+
+        result = document.getElementById("result");
+        result.innerText = operator;
+
+
         // perform an addition
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
+
+        number1 = document.getElementById("op-one");
+        number2 = document.getElementById("op-two");
+
+        operator = parseInt(number1.value) - parseInt(number2.value);
+
+        console.log(operator);
+
+
+        result = document.getElementById("result");
+        result.innerText = operator;
         // perform an substraction
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
+
+        number1 = document.getElementById("op-one");
+        number2 = document.getElementById("op-two");
+
+        operator = parseInt(number1.value) * parseInt(number2.value);
+
+        console.log(operator);
+
+
+        result = document.getElementById("result");
+        // result.setAttribute("value", operator);
+        result.value = operator;
+        console.log("howdie partner!");
         // perform an multiplication
     });
 
     document.getElementById("division").addEventListener("click", function() {
+
+        number1 = document.getElementById("op-one");
+        number2 = document.getElementById("op-two");
+
+        operator = parseInt(number1.value) / parseInt(number2.value);
+
+        console.log(operator);
+
+        result = document.getElementById("result");
+        result.innerText = operator;
         // perform an division
     });
 })();
