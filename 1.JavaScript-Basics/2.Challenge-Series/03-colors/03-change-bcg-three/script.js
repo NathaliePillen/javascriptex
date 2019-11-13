01a3001739611eb6257e48006a82fe3eb9e97f6a
nathalie <pillennathalie@gmail.com>
2019-11-13 21:24:45 +0100

    update 3-3 final

--- 1.JavaScript-Basics/2.Challenge-Series/03-colors/03-change-bcg-three/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/03-colors/03-change-bcg-three/script.js
@@ -11,6 +11,20 @@
 
 (function() {
 
+    let pressButton = document.getElementById("run");
+
+    pressButton.addEventListener(("click"), function() {
+        let x = Math.floor(Math.random() * 256);
+        let y = Math.floor(Math.random() * 256);
+        let z = Math.floor(Math.random() * 256);
+        let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+        console.log(bgColor);
+
+        document.body.style.background = bgColor;
+
+    })
+
+
     // your code here
 
 })();
