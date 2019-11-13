81ad155db0fa5204bc2c355864b9960d972bd2a0
nathalie <pillennathalie@gmail.com>
2019-11-13 23:38:07 +0100

    update 4-3

--- 1.JavaScript-Basics/2.Challenge-Series/04-dates/03-age-by-select/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/04-dates/03-age-by-select/script.js
@@ -11,6 +11,58 @@
 
 (function() {
 
+    let currentDate = new Date();
+    console.log(currentDate);
+    let currentYear = currentDate.getFullYear();
+    console.log(currentYear);
+    let currentMonth = currentDate.getMonth();
+    console.log(currentMonth);
+    let currentDay = currentDate.getDate();
+    console.log(currentDay);
+
+    let birthDay = document.getElementById("dob-day");
+    console.log(birthDay);
+    let birthMonth = document.getElementById("dob-month");
+    console.log(birthMonth);
+    let birthYear = document.getElementById("dob-year");
+    console.log(birthYear);
+
+    let calculateAge = document.getElementById("run");
+    console.log(calculateAge);
+
+    let age = "calculate age";
+
+    calculateAge.addEventListener(("click"), function() {
+
+        age = currentYear - birthYear.value;
+        console.log(age);
+
+        if (currentMonth < (birthMonth.value)) {
+            age;
+        }
+
+        if (((birthMonth - 1) == currentMonth) && (currentDay < birthDay)) {
+            age;
+        }
+
+
+    })
+
+    document.getElementById("run").innerHTML = age;
+
+
+
+
+    // if (today_month < (birth_month - 1)) {
+    //     age--;
+    // }
+    //if (((birth_month - 1) == today_month) && (today_day < birth_day)) {
+    //    age--;
+    //}
+    //return age;
+
+
+
     // your code here
 
 })();
