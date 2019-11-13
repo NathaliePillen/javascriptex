9069ac31247b5f708cb06da7eed691fd86b3e48a
nathalie <pillennathalie@gmail.com>
2019-11-13 11:59:25 +0100

    update 2-4

--- 1.JavaScript-Basics/2.Challenge-Series/02-maths/04-sort-numbers/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/02-maths/04-sort-numbers/script.js
@@ -14,8 +14,22 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", function() {
+        var array1 = [numbers.value];
+        console.log(array1);
+        document.getElementById("numbers").innerHTML = array1;
+        console.log(array1);
+
+        function myFunction() {
+            array1.sort(function(a, b) {
+                return a - b
+            });
+            console.log(array1);
+            document.getElementById("numbers").innerHTML = array1;
+        }
+
 
         // your code here
+
 
     });
 
