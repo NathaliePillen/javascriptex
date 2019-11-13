82edb46d10d07660fb0e88faf932689cc47382ba
nathalie <pillennathalie@gmail.com>
2019-11-13 14:51:26 +0100

    update 2-6 final

--- 1.JavaScript-Basics/2.Challenge-Series/02-maths/06-fizzbuzz/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/02-maths/06-fizzbuzz/script.js
@@ -11,6 +11,22 @@
 
 (function() {
 
+    for (var i = 1; i <= 100; i++) {
+        //console.log(i);
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("fizzbuzz")
+        } else if (i % 3 == 0) {
+            console.log("fizz")
+        } else if (i % 5 == 0) {
+            console.log("buzz")
+        } else
+            console.log(i);
+
+    }
+
+
+
     // your code here
 
 })();
